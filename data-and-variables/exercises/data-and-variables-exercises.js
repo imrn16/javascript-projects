b18f4d5e287c47abae3efc361cb4340dde51fda7
imrn16 <151978120+imrn16@users.noreply.github.com>
2024-06-13 18:30:34 -0500

    modified:   data-and-variables/exercises/data-and-variables-exercises.js

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,16 +1,37 @@
 // Declare and assign the variables below
 let name = "Determination";
 let speed = 17500;
-let marsDistance = 225000000;
-let moonDistance = 384400;
-let mph = 0.621;
+let marsDistanceK = 225000000;
+let moonDistanceK = 384400;
+let mpk = 0.621;
+
 // Use console.log to print the 'typeof' each variable. Print one item per line.
 console.log(typeof name);
-console.log(3);
+console.log(typeof speed);
+console.log(typeof marsDistanceK);
+console.log(typeof moonDistanceK);
+console.log(typeof mpk);
+
 // Calculate a space mission below
+
+let milesToMars = marsDistanceK * mpk;
+
+let hoursToMars = milesToMars / speed;
+
+let daysToMars = hoursToMars / 24;
 
 // Print the results of the space mission calculations below
 
+console.log(daysToMars);
+
 // Calculate a trip to the moon below
 
+let milesToMoon = moonDistanceK * mpk;
+
+let hoursToMoon = milesToMoon / speed;
+
+let daysToMoon = hoursToMoon / 24;
+
 // Print the results of the trip to the moon below
+
+console.log(daysToMoon);
