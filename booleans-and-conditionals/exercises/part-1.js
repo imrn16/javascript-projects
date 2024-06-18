c650a283afbfa6ed95e6a730567818e2d8f1bf59
imrn16 <151978120+imrn16@users.noreply.github.com>
2024-06-17 20:34:09 -0500

    modified:   booleans-and-conditionals/studio/data-variables-conditionals.js

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -1,11 +1,16 @@
 // Declare and initialize the variables for exercise 1 here:
-
+const engineIndicatorLight = "red blinking";
+const spaceSuitsOn = true;
+const shuttleCabinReady = true;
+const crewStatus = spaceSuitsOn && shuttleCabinReady;
+const computerStatusCode = 200;
+const shuttleSpeed = 15000;
 // BEFORE running the code, predict what will be printed to the console by the following statements:
 
 if (engineIndicatorLight === "green") {
-   console.log("engines have started");
+	console.log("engines have started");
 } else if (engineIndicatorLight === "green blinking") {
-   console.log("engines are preparing to start");
+	console.log("engines are preparing to start");
 } else {
-   console.log("engines are off");
+	console.log("engines are off");
 }
