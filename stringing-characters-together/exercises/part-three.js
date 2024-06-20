dd66a158912c07d75b5b4fb7d759533c0162fc28
imrn16 <151978120+imrn16@users.noreply.github.com>
2024-06-20 14:05:46 -0500

    modified:   stringing-characters-together/exercises/part-three.js 	modified:   stringing-characters-together/exercises/part-two.js

--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -1,29 +1,34 @@
 //Part Three section one
 
-let language = 'JavaScript';
+let language = "JavaScript";
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
 
-console.log(language.slice(0,1).concat(language.slice(4,5)))
+console.log(language.slice(0, 1).concat(language.slice(4, 5)));
 
 //2. Without using slice(), use method chaining to accomplish the same thing.
 
-console.log(language.charAt(0).concat(language.charAt(4)))
+console.log(language.charAt(0).concat(language.charAt(4)));
 
 //3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
 
-console.log(`The abbreviation for '${language}' is 'JS'`)
+console.log(`The abbreviation for '${language}' is 'JS'`);
 
 //4. Just for fun, try chaining 3 or more methods together, and then print the result.
 
-console.log(language.charAt('0').concat(language.charAt('4')).concat(language.charAt('7')))
+console.log(language.charAt("0").concat(language.charAt("4")).concat(language.charAt("7")));
 
 //Part Three section Two
 
 //1. Use the string methods you know to print 'Title Case' from the string 'title case'.
 
-let notTitleCase = 'title case';
+let notTitleCase = "title case";
 
-console.log(notTitleCase.charAt(0).toUpperCase())
+console.log(notTitleCase.charAt(0).toUpperCase());
 
-console.log(notTitleCase.split(' ').map(e => e.charAt(0).toUpperCase() + e.substring(1)).join(' '))
+console.log(
+	notTitleCase
+		.split(" ")
+		.map((e) => e.charAt(0).toUpperCase() + e.substring(1))
+		.join(" ")
+);
