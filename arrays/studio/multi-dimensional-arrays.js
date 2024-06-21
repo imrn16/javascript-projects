45b2949cc6baa9587dc58bf95aa78bc46c56b126
imrn16 <151978120+imrn16@users.noreply.github.com>
2024-06-20 19:55:27 -0500

    modified:   arrays/studio/array-string-conversion/array-testing.js 	modified:   arrays/studio/multi-dimensional-arrays.js 	new file:   arrays/studio/node_modules/.package-lock.json 	new file:   arrays/studio/node_modules/readline-sync/LICENSE-MIT 	new file:   arrays/studio/node_modules/readline-sync/README-Deprecated.md 	new file:   arrays/studio/node_modules/readline-sync/README.md 	new file:   arrays/studio/node_modules/readline-sync/lib/encrypt.js 	new file:   arrays/studio/node_modules/readline-sync/lib/read.cs.js 	new file:   arrays/studio/node_modules/readline-sync/lib/read.ps1 	new file:   arrays/studio/node_modules/readline-sync/lib/read.sh 	new file:   arrays/studio/node_modules/readline-sync/lib/readline-sync.js 	new file:   arrays/studio/node_modules/readline-sync/package.json 	new file:   arrays/studio/package-lock.json 	modified:   arrays/studio/package.json 	modified:   arrays/studio/string-modification.js

--- arrays/studio/multi-dimensional-arrays.js
+++ arrays/studio/multi-dimensional-arrays.js
@@ -5,10 +5,47 @@ let sleepAids = "blankets,pillows,eyepatches,alarm clocks";
 
 //1) Use split to convert the strings into four cabinet arrays. Alphabetize the contents of each cabinet.
 
+food = food.split(',').sort()
+equipment = equipment.split(',').sort()
+pets = pets.split(',').sort()
+sleepAids = sleepAids.split(',').sort()
+
+console.log(food)
+console.log(equipment)
+console.log(pets)
+console.log(sleepAids)
+
+
+
 //2) Initialize a cargoHold array and add the cabinet arrays to it. Print cargoHold to verify its structure.
+
+let cargoHold = [food, equipment, pets, sleepAids]
+console.log(cargoHold)
 
 //3) Query the user to select a cabinet (0 - 3) in the cargoHold.
 
+let num = input.question("Please select a number between 0 and 3.")
+
 //4) Use bracket notation and a template literal to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
 
+if (num < 3 && num > 0) {
+    console.log(`The cargo you have selected contains ${cargoHold[num]}`)
+} else {
+    console.log ('Invalid number')
+}
+
 //5) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
+
+let str = ''
+
+if (num < 3 && num > 0) {
+    str = input.question("Which item are you searching for?")
+    if (cargoHold.toLowerCase().includes(str.toLowerCase()) {
+        console.log (`Cabinet DOES contain ${str}`)
+    } else {
+        console.log (`Cabinet DOES NOT contain ${str}`)
+    }
+    )
+} else {
+    console.log ('Invalid number')
+}
--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -1,11 +1,26 @@
-const input = require('readline-sync');
+const input = require("readline-sync");
 let str = "LaunchCode";
+let num = 3;
 
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
 
+let strTwo = str.split("").slice(0, num).join("");
+strTwo = str.split("").slice(num).join("").concat(strTwo);
+console.log(strTwo);
+
 //Use a template literal to print the original and modified string in a descriptive phrase.
+
+console.log(`The first 3 letters from the string '${str}' were removed and appended to the end, creating '${strTwo}'.`);
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
 
+num = input.question("Enter the number of letters which you would like to move from the front to the end of the word.");
+
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+
+if (num > str.length || num < 0) {
+    console.log(`The number provided (${num}) was too long or was invalid. A default of '3' was selected`)
+    num = 3;
+
+} else return num;
