95c9cd4a2aa1510d8b5631e863120acfcfbdf02a
imrn16 <151978120+imrn16@users.noreply.github.com>
2024-06-27 15:52:48 -0500

    modified:   functions/studio/studio-functions.js 	modified:   functions/try-it/reverse.js

--- functions/studio/studio-functions.js
+++ functions/studio/studio-functions.js
@@ -9,6 +9,15 @@
 // 5. Use console.log(reverseCharacters(myVariableName)); to call the function and verify that it correctly reverses the characters in the string.
 // 6. Optional: Use method chaining to reduce the lines of code within the function.
 
+function reverseCharacters(str) {
+		return str.split("").reverse().join("");
+	}
+
+let word = "apple";
+
+console.log(reverseCharacters(word));
+
+
 // Part Two: Reverse Digits
 
 // 1. Add an if statement to reverseCharacters to check the typeof the parameter.
@@ -16,6 +25,18 @@
 // 3. If typeof is ’number’, convert the parameter to a string, reverse the characters, then convert it back into a number.
 // 4. Return the reversed number.
 // 5. Be sure to print the result returned by the function to verify that your code works for both strings and numbers. Do this before moving on to the next exercise.
+
+function reverseCharacters2(str) {
+	if (typeof str == "string") {
+		return str.split("").reverse().join("");
+	} else {
+		return str.toString().split("").reverse().join("");
+	}
+}
+
+let num = "123";
+
+console.log(reverseCharacters2(num));
 
 // Part Three: Complete Reversal - Create a new function with one parameter, which is the array we want to change. The function should:
 
@@ -29,6 +50,15 @@
 let arrayTest1 = ['apple', 'potato', 'Capitalized Words'];
 let arrayTest2 = [123, 8897, 42, 1168, 8675309];
 let arrayTest3 = ['hello', 'world', 123, 'orange'];
+
+function reverseArray(arr) {
+	return arr.reverse().map((e) => {
+		return e.toString().split("").reverse().join("");
+	});
+}
+
+console.log(reverseArray(arrayTest3));
+
 
 // Bonus Missions
 
--- functions/try-it/reverse.js
+++ functions/try-it/reverse.js
@@ -3,25 +3,3 @@ function reverse(str) {
 	let reversedLettersArray = lettersArray.reverse();
 	return reversedLettersArray.join("");
 }
-
-function reverseCharacters(str) {
-	if (typeof str == "string") {
-		return str.split("").reverse().join("");
-	} else {
-		return str.toString().split("").reverse().join("");
-	}
-}
-
-let word = "123";
-
-console.log(reverseCharacters(word));
-
-function reverseArray(arr) {
-	return arr.reverse().map((e) => {
-		return e.toString().split("").reverse().join("");
-	});
-}
-
-let array = ["apple", "potato", "Capitalized Words"];
-
-console.log(reverseArray(array));
