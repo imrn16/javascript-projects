1542aad49f1ba30e35a708d03046f59c71947c41
imrn16 <151978120+imrn16@users.noreply.github.com>
2024-06-27 15:48:15 -0500

    modified:   functions/try-it/reverse.js

--- functions/try-it/reverse.js
+++ functions/try-it/reverse.js
@@ -1,5 +1,27 @@
 function reverse(str) {
-   let lettersArray = str.split('');
-   let reversedLettersArray = lettersArray.reverse();
-   return reversedLettersArray.join('');
+	let lettersArray = str.split("");
+	let reversedLettersArray = lettersArray.reverse();
+	return reversedLettersArray.join("");
 }
+
+function reverseCharacters(str) {
+	if (typeof str == "string") {
+		return str.split("").reverse().join("");
+	} else {
+		return str.toString().split("").reverse().join("");
+	}
+}
+
+let word = "123";
+
+console.log(reverseCharacters(word));
+
+function reverseArray(arr) {
+	return arr.reverse().map((e) => {
+		return e.toString().split("").reverse().join("");
+	});
+}
+
+let array = ["apple", "potato", "Capitalized Words"];
+
+console.log(reverseArray(array));
