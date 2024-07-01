bf7c717dd3f7ee884e009e577ee5614269aa672f
imrn16 <151978120+imrn16@users.noreply.github.com>
2024-07-01 18:35:08 -0500

    modified:   more-on-functions/exercises/practice-your-skills.js 	modified:   more-on-functions/exercises/raid-a-shuttle.js

--- more-on-functions/exercises/practice-your-skills.js
+++ more-on-functions/exercises/practice-your-skills.js
@@ -6,10 +6,33 @@ b) If passed a string, return the string “ARRR!”
 c) Be sure to test your function before moving on the next part. 
 */
 
-/* Add to your code! Use your fuction and the map method to change an array  as follows:
+let anonymous = function (prop) {
+	if (typeof prop === "number") {
+		return prop * 3;
+	} else if (typeof prop === "string") {
+		return "ARRR!";
+	}
+};
+
+console.log(anonymous("3"));
+
+/* Add to your code! Use your function and the map method to change an array  as follows:
 a) Triple any the numbers.
 b) Replace any strings with “ARRR!”
 c) Print the new array to confirm your work.
 */
 
-let arr = ['Elocution', 21, 'Clean teeth', 100];
+let anonArr = function (val) {
+	return val.map((indiv) => {
+		if (typeof indiv === "number") {
+			indiv = indiv * 3;
+		} else if (typeof indiv === "string") {
+			indiv = "ARRR!";
+		}
+		return indiv;
+	});
+};
+
+let arr = ["Elocution", 21, "Clean teeth", 100];
+
+console.log(anonArr(arr));
--- more-on-functions/exercises/raid-a-shuttle.js
+++ more-on-functions/exercises/raid-a-shuttle.js
@@ -1,32 +1,41 @@
 function checkFuel(level) {
-  if (level > 100000){
-    return 'green';
-  } else if (level > 50000){
-    return 'yellow';
-  } else {
-    return 'red';
-  }
+	if (level > 100000) {
+		return "green";
+	} else if (level > 50000) {
+		return "yellow";
+	} else {
+		return "red";
+	}
 }
 
-function holdStatus(arr){
-  if (arr.length < 7) {
-    return `Spaces available: ${7-arr.length}.`;
-  } else if (arr.length > 7){
-    return `Over capacity by ${arr.length-7} items.`;
-  } else {
-    return "Full";
-  }
+let fuelChecker = function (val) {
+	if (checkFuel(val) === "green") {
+		return val - 100001;
+	} else if (checkFuel(val) === "yellow") {
+		return val - 50001;
+	} else return val;
+};
+
+function holdStatus(arr) {
+	if (arr.length < 7) {
+		return `Spaces available: ${7 - arr.length}.`;
+	} else if (arr.length > 7) {
+		return `Over capacity by ${arr.length - 7} items.`;
+	} else {
+		return "Full";
+	}
 }
 
 let fuelLevel = 200000;
-let cargoHold = ['meal kits', 'space suits', 'first-aid kit', 'satellite', 'gold', 'water', 'AE-35 unit'];
+let cargoHold = ["meal kits", "space suits", "first-aid kit", "satellite", "gold", "water", "AE-35 unit"];
+fuelChecker(fuelLevel);
 
 console.log("Fuel level: " + checkFuel(fuelLevel));
 console.log("Hold status: " + holdStatus(cargoHold));
 
 /* Steal some fuel from the shuttle:
  */
- 
+
 //a). Define an anonymous function and set it equal to a variable with a normal, non-suspicious name. The function takes one parameter. This will be the fuel level on the shuttle.
 
 //b). You must siphon off fuel without alerting the TAs. Inside your function, you want to reduce the fuel level as much as possible WITHOUT changing the color returned by the checkFuel function.
@@ -38,6 +47,12 @@ console.log("Hold status: " + holdStatus(cargoHold));
 /* Next, liberate some of that glorious cargo.
  */
 
+let cargoInspector = function (arr) {
+	let newItem = arr.splice(3, 2, "band-aids", "clippers");
+	cargoHold = arr;
+	return newItem;
+};
+
 //a). Define another anonymous function with an array as a parameter, and set it equal to another innocent variable.
 
 //b). You need to swipe two items from the cargo hold. Choose well. Stealing water ain’t gonna get us rich. Put the swag into a new array and return it from the function.
@@ -48,9 +63,16 @@ console.log("Hold status: " + holdStatus(cargoHold));
 
 /* Finally, you need to print a receipt for the accountant. Don’t laugh! That genius knows MATH and saves us more gold than you can imagine.
  */
- 
+
 //a). Define a function called irs that can take fuelLevel and cargoHold as arguments.
-	
+
 //b). Call your anonymous fuel and cargo functions from within irs.
 
 //c). Use a template literal to return, "Raided _____ kg of fuel from the tanks, and stole ____ and ____ from the cargo hold."
+
+let irs = function (fuel, cargo) {
+	let arr = cargoInspector(cargo);
+	return `Raided ${fuelChecker(fuel)} kg of fuel from the tanks, and stole ${arr[0]} and ${arr[1]} from the cargo hold.`;
+};
+
+console.log(irs(fuelLevel, cargoHold));
