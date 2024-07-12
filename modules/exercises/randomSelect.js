4a5a3a3cbe436e6f1bee7f7b29fb7479ef97468f
imrn16 <151978120+imrn16@users.noreply.github.com>
2024-07-11 20:21:32 -0500

    modified:   modules/exercises/ScoreCalcs/averages.js 	modified:   modules/exercises/display.js modified:   modules/exercises/index.js 	modified:   modules/exercises/randomSelect.js 	modified:   modules/exporting-modules/index.js modified:   modules/exporting-modules/practiceExports.js

--- modules/exercises/randomSelect.js
+++ modules/exercises/randomSelect.js
@@ -1,5 +1,9 @@
-function randomFromArray(arr){
-  //Your code here to select a random element from the array passed to the function.
+function randomFromArray(arr) {
+	//Your code here to select a random element from the array passed to the function.
+	let x = Math.floor(Math.random() * arr.length);
+	return arr[x];
 }
 
 //TODO: Export the randomFromArray function.
+
+module.exports = randomFromArray;
--- modules/exporting-modules/index.js
+++ modules/exporting-modules/index.js
@@ -1,1 +1,4 @@
 // Import the modules exported from practiceExports.js below:
+const evenOrOdd = require("../exporting-modules/practiceExports");
+const isPalindrome = require("../exporting-modules/practiceExports");
+const randomArrayElement = require("../exporting-modules/practiceExports");
